887e472f763b81bddcb1abd6c483f406ad7301d2
Jared Langson <langson.jared@gmail.com>
2022-09-25 15:19:48 -0400

    Add dummy project.clj so cursive recognizes shadow-cljs

---
+++ project.clj
@@ -0,0 +1,7 @@
+; dummy file so cursive recognizes shadow-cljs https://shadow-cljs.github.io/docs/UsersGuide.html#_cursive
+(defproject markdownify "0.0.0"
+  :dependencies
+  [[thheller/shadow-cljs "2.20.2"]]
+
+  :source-paths
+  ["src"])
