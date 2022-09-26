af27d1b927084e5c66530f5dc97801dcc0b3d684
Jared Langson <langson.jared@gmail.com>
2022-09-25 21:55:36 -0400

    Add buttons to copy md and html

--- src/main/markdownify/main.cljs
+++ src/main/markdownify/main.cljs
@@ -16,6 +16,24 @@
   (.makeHtml showdown-converter md))
 
 
+;; https://hackernoon.com/copying-text-to-clipboard-with-javascript-df4d4988697f
+(defn copy-to-clipboard [s]
+  (let [el (.createElement js/document "textarea")
+        selected (when (pos? (-> js/document .getSelection .-rangeCount))
+                   (-> js/document .getSelection (.getRangeAt 0)))]
+    (set! (.-value el) s)
+    (.setAttribute el "readonly" "")
+    (set! (-> el .-style .-position) "absolute")
+    (set! (-> el .-style .-left) "-9999px")
+    (-> js/document .-body (.appendChild el))
+    (.select el)
+    (.execCommand js/document "copy")
+    (-> js/document .-body (.removeChild el))
+    (when selected
+      (-> js/document .getSelection .removeAllRanges)
+      (-> js/document .getSelection (.addRange selected)))))
+
+
 ;reagent component
 (defn app []
   [:div
@@ -28,22 +46,21 @@
      [:textarea                                             ;hashmap on html element defines attributes of <textarea>
       {:on-change #(reset! markdown (-> % .-target .-value))
        :value     @markdown
-       :style { :height "500px"
-               :width "100%"}}]]                  ;displays atom on reload in textbox
-
+       :style     {:height "500px"
+                   :width  "100%"}}]
+     [:button
+      {:on-click #(copy-to-clipboard @markdown)}
+      "Copy Markdown"]]
 
     [:div
      {:style {:flex "1"
               :padding-left "3em"}}
      [:h2 "HTML Preview"]
-     [:div { :style {:height "500px"}
-            :dangerouslySetInnerHTML {:__html (md->html @markdown)}}]]]]) ;renders md as html
-
-
-
-
-
-
+     [:div {:style                   {:height "500px"}
+            :dangerouslySetInnerHTML {:__html (md->html @markdown)}}]
+     [:button
+      {:on-click #(copy-to-clipboard (md->html @markdown))}
+      "Copy HTML!!!"]]]])
 
 
 
